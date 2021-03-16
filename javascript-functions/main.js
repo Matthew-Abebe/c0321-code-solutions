1fe839c22d9cb6d5aa20f47bf17c4d86f503564a
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-16 15:58:53 -0700

    Completed javascript-functions.

---
+++ javascript-functions/main.js
@@ -0,0 +1,32 @@
+function convertMinutesToSeconds(minutes) {
+  var numberOfSeconds = minutes * 60;
+  return numberOfSeconds;
+}
+console.log('Minutes to seconds:', convertMinutesToSeconds(5));
+
+function greet(name) {
+  return ('Hey, ' + name);
+}
+console.log('Greeting:', greet('Beavis'));
+
+function getArea(width, height) {
+  var area = width * height;
+  return area;
+}
+console.log('Area:', getArea(17, 42));
+
+function getFirstName(person) {
+  person = {
+    firstName: 'Michael',
+    lastName: 'Jordan'
+  };
+  return person.firstName;
+}
+console.log('First name:', getFirstName({ firstName: 'Michael', lastName: 'Jordan' }));
+
+function getLastElementArray(array) {
+  var arrayCount = array.length;
+  var lastArrayEl = arrayCount - 1;
+  return array[lastArrayEl];
+}
+console.log('Last element of array:', getLastElementArray(['Milk', 'Cheese', 'Eggs', 'Bread']));
