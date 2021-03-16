74a1205e207723f2d48fe810414ddcd98314d4ef
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-16 11:51:03 -0700

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,49 @@
+var orderHistory = [
+  {
+    productDescription: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    itemPrice: '$31.55',
+    totalPrice: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '#114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    returnWindow: 'Return window closed on Sep 7, 2020',
+    deliveryDate: 'Aug 8, 2020'
+  },
+
+  {
+    productDescription: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    itemPrice: '$41.33',
+    totalPrice: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '#113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    returnWindow: 'Return window closed on Aug 19, 2020',
+    deliveryDate: 'Jul 20, 2020'
+  },
+
+  {
+    productDescription: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    itemPrice: '$15.98',
+    totalPrice: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '#114-2875557-9059409',
+    orderPlaced: 'Jul 4, 2020',
+    returnWindow: 'Return window closed on Aug 5, 2020',
+    deliveryDate: 'Delivered Jul 7, 2020'
+  },
+
+  {
+    productDescription: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    author: 'Faroult, Stephanie',
+    itemPrice: ['$94.95, $33.99'],
+    totalPrice: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '#113-2883177-2648248',
+    orderPlaced: 'July 3 2020',
+    returnWindow: ['Return window closed on Aug 4, 2020', 'Return window closed on Aug 4, 2020'],
+    deliveryDate: 'Jul 5, 2020'
+  }
+
+];
