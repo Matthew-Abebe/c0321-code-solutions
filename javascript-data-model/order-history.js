f692e0b53711ee55092f26d1a5c2f8ea4e54f703
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-16 16:07:35 -0700

    Moved properties up to larger order object.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,30 +1,29 @@
 var orderHistory = [
   {
+    shipTo: 'JS Masher',
+    orderNumber: '#114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    returnWindow: 'Return window closed on Sep 7, 2020',
+    deliveryDate: 'Aug 8, 2020',
     items: [{
       productDescription: 'JavaScript for impatient programmers',
       author: 'Rauschmayer, Dr. Axel',
       itemPrice: '$31.55',
-      totalPrice: '$34.00',
-      shipTo: 'JS Masher',
-      orderNumber: '#114-3941689-8772232',
-      orderPlaced: 'August 4, 2020',
-      returnWindow: 'Return window closed on Sep 7, 2020',
-      deliveryDate: 'Aug 8, 2020'
+      totalPrice: '$34.00'
     }]
   },
 
   {
+    shipTo: 'JS Masher',
+    orderNumber: '#113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    returnWindow: 'Return window closed on Aug 19, 2020',
+    deliveryDate: 'Jul 20, 2020',
     items: [{
       productDescription: 'The Timeless Way of Building',
       author: 'Alexander, Christopher',
       itemPrice: '$41.33',
-      totalPrice: '$44.53',
-      shipTo: 'JS Masher',
-      orderNumber: '#113-9984268-1280257',
-      orderPlaced: 'July 19, 2020',
-      returnWindow: 'Return window closed on Aug 19, 2020',
-      deliveryDate: 'Jul 20, 2020'
-
+      totalPrice: '$44.53'
     }]
   },
 
