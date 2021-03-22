d633e86d9a3b37b9534643b3165d4fe333b2f0d5
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-22 07:21:37 -0700

    Working on getpropertyvalues.

---
+++ javascript-functions-practice/get-property-value.js
@@ -0,0 +1,14 @@
+/* exported getPropertyValue */
+
+function getPropertyValue(object, string) {
+  object = {
+    name: '',
+    occupation: '',
+    birthplace: ''
+  };
+
+  var key = '';
+  var output = object[key];
+
+  return output;
+}
