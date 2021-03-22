aa9d279571caa11d43b3101fcf30f66914c374ba
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-22 11:59:15 -0700

    added full element to console.dir.

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,15 +1,15 @@
 console.log('hello, world!');
 
 console.log(document.querySelector('h1'));
-console.dir('h1');
+console.dir(document.querySelector('h1'));
 
 console.log(document.querySelector('#explanation'));
-console.dir('#explanation');
+console.dir(document.querySelector('#explanation'));
 
 console.log(document.querySelector('.hint'));
-console.dir('.hint');
+console.dir(document.querySelector('.hint'));
 
 console.log(document.querySelector('p'));
-console.dir('p');
+console.dir(document.querySelector('p'));
 
 console.log(document.querySelectorAll('.example-link'));
