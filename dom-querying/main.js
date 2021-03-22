63b4a873f724f36dafe40d5f8f9c0c5ea0a81ca3
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-22 11:21:45 -0700

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,15 @@
+console.log('hello, world!');
+
+console.log(document.querySelector('h1'));
+console.dir('h1');
+
+console.log(document.querySelector('#explanation'));
+console.dir('#explanation');
+
+console.log(document.querySelector('.hint'));
+console.dir('.hint');
+
+console.log(document.querySelector('p'));
+console.dir('p');
+
+console.log(document.querySelectorAll('.example-link'));
