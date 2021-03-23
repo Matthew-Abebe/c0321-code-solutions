381fc56d2c251e05f787db8bc24ef3cc3f4ffe03
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-23 12:03:00 -0700

    Completed dom-events exercise.

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,6 +1,26 @@
+var button = document.querySelector('.click-button');
+button.addEventListener('click', handleClick);
+
 function handleClick(event) {
   console.log('button clicked');
-  console.log(event);
+  console.log('event: ', event);
+  console.log('eventTarget: ', event.target);
 }
 
-handleClick();
+var hoverButton = document.querySelector('.hover-button');
+hoverButton.addEventListener('mouseover', handleMouseOver);
+
+function handleMouseOver(event) {
+  console.log('button hovered');
+  console.log('event: ', event);
+  console.log('eventTarget: ', event.target);
+}
+
+var doubleClickButton = document.querySelector('.double-click-button');
+doubleClickButton.addEventListener('dblclick', handleDoubleClick);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event: ', event);
+  console.log('eventTarget: ', event.target);
+}
