3cc64ec02d603b4f8eeb14bc2d77c1ab0ed05219
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-24 15:51:02 -0700

    used element property to access input elements.

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -5,10 +5,14 @@ contactForm.addEventListener('submit', handleSubmit);
 function handleSubmit(event, obj) {
   event.preventDefault();
 
+  var name = contactForm.elements.name.value;
+  var email = contactForm.elements.email.value;
+  var message = contactForm.elements.message.value;
+
   var controlFormsObj = {
-    user_name: contactForm.name.value,
-    user_email: contactForm.email.value,
-    user_message: contactForm.message.value
+    user_name: name,
+    user_email: email,
+    user_message: message
   };
 
   contactForm.reset();
