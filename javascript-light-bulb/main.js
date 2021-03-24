8b62b6adece8756828363c244f6bf23292b23468
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-24 15:40:00 -0700

    made light toggle-able.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,16 +1,18 @@
 var button = document.getElementById('light-bulb');
 var background = document.querySelector('body');
+var isBulbOn;
 
 button.addEventListener('click', handleClick);
-button.addEventListener('dblclick', handleDoubleClick);
 
 function handleClick(event) {
-  button.className = 'light-on';
-  background.className = 'background-light-on';
 
-}
-
-function handleDoubleClick(event) {
-  button.className = 'light-off';
-  background.className = 'background-light-off';
+  isBulbOn = !isBulbOn;
+  if (isBulbOn === true) {
+    button.className = 'light-on';
+    background.className = 'background-light-on';
+  }
+  if (isBulbOn === false) {
+    button.className = 'light-off';
+    background.className = 'background-light-off';
+  }
 }
