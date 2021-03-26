39620d3376fef70ba13190b6152625e85716ee99
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-26 16:39:52 -0700

    Finished addSuffixToAll function.

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -1,1 +1,10 @@
 /* exported addSuffixToAll */
+
+function addSuffixToAll(words, suffix) {
+
+  for (var i = 0; i < words.length; i++) {
+    words[i] += suffix;
+  }
+
+  return (words);
+}
--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -1,1 +1,11 @@
 /* exported getIndexes */
+
+function getIndexes(array) {
+  var indexes = [];
+
+  for (var i = 0; i < array.length; i++) {
+    var index = array.indexOf(array[i]);
+    indexes.push(index);
+  }
+  return (indexes);
+}
