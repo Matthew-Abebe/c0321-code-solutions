bcc2db0998266c25e1fc85e09561c9a56552b6ec
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-26 13:10:50 -0700

    Finished first loop.

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -2,12 +2,9 @@
 
 function countdown(number) {
   var numbersArr = [];
-  // initializer variable for loop starts at a given number
-  // condition i is set between 0 and a given number
-  // expression decrements i every time loop is run
-  for (var i = number; i <= number && i >= 0; i--) {
-    // number is decremented every time loop is run and number is pushed into numbers arr
-    number--;
-    numbersArr.push(number);
+
+  for (var i = number; i >= 0; i--) {
+    numbersArr.push(i);
   }
+  return (numbersArr);
 }
