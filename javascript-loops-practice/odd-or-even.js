436dc59fb39006a86b86ff289be9ea11654625c5
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-27 09:03:34 -0700

    Completed javascript-loops-practice challenge.

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -4,9 +4,8 @@ function oddOrEven(numbers) {
   var isEven = 'even';
   var isOdd = 'odd';
   var newArr = [];
-
   for (var i = 0; i < numbers.length; i++) {
-    if (numbers[i] / 2 === 0) {
+    if (numbers[i] % 2 === 0) {
       newArr.push(isEven);
     } else {
       newArr.push(isOdd);
