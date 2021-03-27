8c7ea1e936f5ddd13f28d5705545843a4bd97a9d
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-27 09:29:26 -0700

    changes

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,8 +1,21 @@
-var userClicks = 0;
-var button = document.querySelector('.hot-button');
+var clicks = 0;
+var hotButton = document.querySelector('.hot-button');
 var clickCount = document.querySelector('.click-count');
-
-button.addEventListener('click', function () {
-  clickCount = userClicks;
-  return (clickCount);
-});
+hotButton.addEventListener('click', handleClick);
+function handleClick(event) {
+  clicks++;
+  clickCount.textContent = clicks;
+  if (clicks < 4) {
+    hotButton.className = 'hot-button cold';
+  } else if (clicks < 7) {
+    hotButton.className = 'hot-button cool';
+  } else if (clicks < 10) {
+    hotButton.className = 'hot-button tepid';
+  } else if (clicks < 13) {
+    hotButton.className = 'hot-button warm';
+  } else if (clicks < 16) {
+    hotButton.className = 'hot-button hot';
+  } else {
+    hotButton.className = 'hot-button nuclear';
+  }
+}
