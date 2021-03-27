43d195676a776193969735d5093187c74ed70a8b
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-27 12:25:53 -0700

    Completed string-manipulation-1.

---
+++ string-manipulation-1/src/get-words.js
@@ -0,0 +1,16 @@
+/* exported getWords */
+
+function getWords(string) {
+
+  var arr = [];
+
+  if (string === '') {
+    return arr;
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    var word = string.split(' ');
+
+  }
+  return (word);
+}
