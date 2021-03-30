25475cc97528e2d8443f9d57bee687ad53fea17a
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-29 21:13:13 -0700

    Added charLowerCase variable for isVowel function.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,7 +1,11 @@
 /* exported isVowel */
 
 function isVowel(char) {
-  if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U' || char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+  var charLowerCase = char.toLowerCase();
+
+  if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+    return true;
+  } else if (charLowerCase === 'a' || charLowerCase === 'e' || charLowerCase === 'i' || charLowerCase === 'o' || charLowerCase === 'u') {
     return true;
   } else {
     return false;
