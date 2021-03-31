a10fd54b747838360bf90a032420b23faa5a3f1d
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-30 19:09:47 -0700

    removed unecessary condition.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -3,9 +3,7 @@
 function isVowel(char) {
   var charLowerCase = char.toLowerCase();
 
-  if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
-    return true;
-  } else if (charLowerCase === 'a' || charLowerCase === 'e' || charLowerCase === 'i' || charLowerCase === 'o' || charLowerCase === 'u') {
+  if (charLowerCase === 'a' || charLowerCase === 'e' || charLowerCase === 'i' || charLowerCase === 'o' || charLowerCase === 'u') {
     return true;
   } else {
     return false;
