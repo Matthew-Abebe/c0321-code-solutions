947676c1388688c60939f47301b0324d42762912
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-03 19:09:58 -0700

    Starting javascript modal exercise.

---
+++ javascript-modal/main.js
@@ -0,0 +1,7 @@
+var openModalButton = document.querySelector('.open-modal');
+
+openModalButton.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  // console.log('hello');
+}
