6d2a8dabc79f19f42cf399b43cb04c794f6831ce
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-05 15:41:53 -0700

    Compact function omit NaN from arrays.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,20 +2,11 @@
 
 function compact(array) {
   var newArr = [];
-
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== 0 && typeof array[i] !== 'undefined' && array[i] !== '') {
+    if (array[i] !== false && array[i] !== 0 && typeof array[i] !== 'undefined' && array[i] !== '' && array[i] !== null && !isNaN(array[i])
+    ) {
       newArr.push(array[i]);
     }
   }
   return (newArr);
 }
-
-// function compact(array) {
-//   var newArr = [];
-
-//   for (var i = 0; i < array.length; i++) {
-//     if (array.includes();
-//   }
-//   return (newArr);
-// }
