bad9df1e96534197b8f3f658b85d40e4fc425315
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-05 15:54:03 -0700

    Completed array-manipulation-1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,7 +3,7 @@
 function compact(array) {
   var newArr = [];
   for (var i = 0; i < array.length; i++) {
-    if (array[i] !== false && array[i] !== 0 && typeof array[i] !== 'undefined' && array[i] !== '' && array[i] !== null && !isNaN(array[i])
+    if (array[i] !== false && array[i] !== 0 && typeof array[i] !== 'undefined' && array[i] !== '' && array[i] !== null && !Number.isNaN(array[i])
     ) {
       newArr.push(array[i]);
     }
