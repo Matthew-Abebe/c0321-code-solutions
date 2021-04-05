34b191ad838304881d9ad32ff788b071e242d3af
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-05 12:49:20 -0700

    Working on compact function.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -4,11 +4,18 @@ function compact(array) {
   var newArr = [];
 
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === false || array[i] === isNaN(1 + undefined) || array[i] === 0 || typeof array[i] === 'undefined' || array[i] === '' || isNaN(array[i])) {
-      delete array[i];
-    } else {
+    if (array[i] !== false && array[i] !== 0 && typeof array[i] !== 'undefined' && array[i] !== '') {
       newArr.push(array[i]);
     }
   }
   return (newArr);
 }
+
+// function compact(array) {
+//   var newArr = [];
+
+//   for (var i = 0; i < array.length; i++) {
+//     if (array.includes();
+//   }
+//   return (newArr);
+// }
