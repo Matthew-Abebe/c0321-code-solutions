59eed0ad501429551fb3a8c19865477384926745
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-07 11:53:55 -0700

    Work for getAccount method.

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,33 @@
 /* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+
+  if (!Number.isInteger(balance) || balance <= 0) {
+    return null;
+  } else {
+    var account = new Account(this.nextAccountNumber, holder);
+    account.deposit(balance);
+    this.accounts.push(account);
+    this.nextAccountNumber++;
+  }
+  return account.number;
+};
+
+Bank.prototype.getAccount = function (number) {
+
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  }
+  return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+
+};
