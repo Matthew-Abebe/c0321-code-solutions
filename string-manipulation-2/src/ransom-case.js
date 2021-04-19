db9da2229c6c2ee9ba3ade0ef19d6887f36ac770
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-19 10:06:50 -0700

    Work for string-manipulation-2.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,14 @@
 /* exported ransomCase */
+
+function ransomCase(string) {
+  string = string.toLowerCase();
+
+  var lettersArr = string.split(' ');
+  var newArr = [];
+
+  for (var i = 0; i < lettersArr.length; i += 2) {
+    newArr.push(lettersArr[i].toUpperCase());
+  }
+  return newArr.join(' ');
+
+}
