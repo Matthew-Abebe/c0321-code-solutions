923d81beca2741dffb44b9f9a18fa22ab9836bbb
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-24 15:49:14 -0700

    Swapping chars.

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -3,4 +3,19 @@
 function swapChars(firstIndex, secondIndex, string) {
   var newArr = string.split('');
 
+  firstIndex = newArr[firstIndex];
+  secondIndex = newArr[secondIndex];
+  var temp;
+
+  temp = firstIndex;
+  firstIndex = secondIndex;
+  secondIndex = temp;
+
+  console.log(firstIndex, secondIndex);
+
+  string = newArr.join('');
+
+  return string;
 }
+
+// Swapping characters but trouble returning string.
