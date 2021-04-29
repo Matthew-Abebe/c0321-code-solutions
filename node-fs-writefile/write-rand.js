fb3b42b4a1744e49f10c6209015807142ee6b250
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-29 11:29:32 -0700

    work.

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const data = Math.random().toString();
 
-fs.writeFile('random-text.txt', data, err => {
+fs.writeFile('random.txt', data, err => {
   if (err) throw err;
   console.log('The file has been saved!');
 });
