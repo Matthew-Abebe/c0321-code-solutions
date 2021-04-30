99334166ebd1b12ce34308ce0d40d68a9c0e1158
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-04-30 12:51:05 -0700

    Added for-in loop.

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,46 +1,36 @@
 const fs = require('fs');
 fs.readFile('data.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-  // Write result back into data.json
-  if (process.argv[2] === 'create') {
-    // Parse obj
+
+  if (process.argv[2] === 'read') {
+    const parsedData = JSON.parse(data);
+    for (const key in parsedData.notes) {
+      console.log(`${key}: ${parsedData.notes[key]}`);
+    }
+    if (err) throw err;
+  } else if (process.argv[2] === 'create') {
     const obj = JSON.parse(data);
-    // Modify Obj
     obj.notes[obj.nextId] = process.argv[3];
-    // Increment obj.nextId
     obj.nextId++;
-    // Stringify obj
     const result = JSON.stringify(obj, null, 2);
     fs.writeFile('data.json', result, err => {
       if (err) throw err;
       console.log('The note has been saved!');
     });
-  } else if (process.argv[2] === 'remove') {
-    // Parse obj
+  } else if (process.argv[2] === 'delete') {
     const obj = JSON.parse(data);
-    // Modify Obj
     delete obj.notes[process.argv[3]];
-    // Stringify obj
     const result = JSON.stringify(obj, null, 2);
     fs.writeFile('data.json', result, err => {
       if (err) throw err;
       console.log('The note has been removed!');
     });
-  } else if (process.argv[2] === 'replace') {
-    // Parse obj
+  } else if (process.argv[2] === 'update') {
     const obj = JSON.parse(data);
-    // Modify Obj
     obj.notes[process.argv[3]] = process.argv[4];
-
     const result = JSON.stringify(obj, null, 2);
-
     fs.writeFile('data.json', result, err => {
       if (err) throw err;
       console.log('The note has been updated!');
     });
   }
 });
-
-// need to use for loop to find the one that has the id.
-// want to check if the id i am looking at is equal to the id.
