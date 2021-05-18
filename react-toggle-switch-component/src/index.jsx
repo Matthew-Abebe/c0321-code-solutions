486235e98467b6b5bdeaae0347e40a6278067c73
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-05-18 10:27:26 -0700

    work for react-toggle-switch-component.

--- react-toggle-switch-component/src/index.jsx
+++ react-toggle-switch-component/src/index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const toggleSwitch = (
-<label className="switch">
+<label className="switch">ON OFF
   <input type="checkbox" />
-    <span className="slider round"></span>
+  <span className="slider round"></span>
   </label>
-
 );
+
+// function handleChange(event) {
+
+// }
 
 ReactDOM.render(toggleSwitch, document.querySelector('#root'));
