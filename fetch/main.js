3434353a2665f1f4200b9c64c8e1a9240bf77421
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-05-18 11:15:48 -0700

    removed new array.

--- fetch/main.js
+++ fetch/main.js
@@ -1,13 +1,7 @@
-const array = [];
-
 fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
-  .then(data => array.push(data))
-  .then(console.log(array));
-
-const favoritePokemon = [];
+  .then(data => console.log(data));
 
 fetch('https://pokeapi.co/api/v2/pokemon/ditto')
   .then(response => response.json())
-  .then(data => favoritePokemon.push(data))
-  .then(console.log(favoritePokemon));
+  .then(data => console.log(data));
