60bb471f905aba0f38a666b1860c3195c0fd5d1f
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-08-27 07:35:15 -0700

    work for string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,14 +1,23 @@
 /* exported titleCase */
 
 function titleCase(title) {
-  var words = title.split(' ');
-  var wordArr = [];
-  var capitalizedWords = '';
+  var wordsArr = title.split(' ');
+  var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  var words;
 
-  for (let i = 0; i < words.length; i++) {
-    capitalizedWords = words[i].charAt(0).toUpperCase() + words[i].slice(1);
-    wordArr.push(capitalizedWords);
+  for (let i = 0; i < wordsArr.length; i++) {
+    wordsArr[i] = wordsArr[i].charAt(0).toLowerCase() + wordsArr[i].substring(1);
   }
 
-  console.log(wordArr);
+  for (let j = 0; j < wordsArr.length; j++) {
+    for (let m = 0; m < minorWords.length; m++) {
+      if (wordsArr[j] !== minorWords[m]) {
+        wordsArr[j] = wordsArr[j].charAt(0).toUpperCase() + wordsArr[j].substring(1);
+      }
+    }
+  }
+
+  words = wordsArr.join(' ');
+
+  console.log(words);
 }
