c019f1ff2453c10b8e7deae30589dbab5eac2da8
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-08-27 10:13:13 -0700

    work for string-manipulation-3.:

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,22 +2,30 @@
 
 function titleCase(title) {
   var wordsArr = title.split(' ');
-  var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+  // var minorWords = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
   var words;
 
   for (let i = 0; i < wordsArr.length; i++) {
-    wordsArr[i] = wordsArr[i].charAt(0).toLowerCase() + wordsArr[i].substring(1);
+    wordsArr[i] = wordsArr[i].charAt(0).toLowerCase() + wordsArr[i].slice(1);
   }
 
   for (let j = 0; j < wordsArr.length; j++) {
-    for (let m = 0; m < minorWords.length; m++) {
-      if (wordsArr[j] !== minorWords[m]) {
-        wordsArr[j] = wordsArr[j].charAt(0).toUpperCase() + wordsArr[j].substring(1);
-      }
+    if (wordsArr[j] !== 'and' && wordsArr[j] !== 'or' && wordsArr[j] !== 'nor' && wordsArr[j] !== 'but' && wordsArr[j] !== 'a' && wordsArr[j] !== 'an' && wordsArr[j] !== 'the' && wordsArr[j] !== 'as' && wordsArr[j] !== 'at' && wordsArr[j] !== 'by' && wordsArr[j] !== 'for' && wordsArr[j] !== 'in' && wordsArr[j] !== 'of' && wordsArr[j] !== 'on' && wordsArr[j] !== 'per' && wordsArr[j] !== 'to') {
+      wordsArr[j] = wordsArr[j].charAt(0).toUpperCase() + wordsArr[j].slice(1);
     }
+
+    if (wordsArr[j] === 'Javascript' || wordsArr[j] === 'Javascript:') {
+      wordsArr[j] = 'Java' + wordsArr[j].charAt(4).toUpperCase() + wordsArr[j].slice(5);
+    }
+
+    if (wordsArr[j] === 'Api') {
+      wordsArr[j] = wordsArr[j].toUpperCase();
+    }
+
   }
 
   words = wordsArr.join(' ');
 
   console.log(words);
+  return words;
 }
