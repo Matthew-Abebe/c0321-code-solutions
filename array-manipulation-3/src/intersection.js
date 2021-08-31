818ae9ba7fe03fde797aea284a95b98fd2b30357
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-08-31 09:13:54 -0700

    completed array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,13 @@
 /* exported intersection */
+
+function intersection(first, second) {
+  var newArr = [];
+
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      newArr.push(first[i]);
+    }
+  }
+
+  return newArr;
+}
