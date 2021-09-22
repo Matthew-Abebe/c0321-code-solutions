96883ee5b52fd2551eb0be9bddc595bd2d9af8a4
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-09-22 14:28:02 -0700

    made circle for stopwatch.

---
+++ react-stopwatch-component/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stopwatch from './stopwatch';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(
+  <Stopwatch />,
+  root
+);
---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Stopwatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    const stopwatch = <div id="stopwatch"></div>;
+    return stopwatch;
+  }
+}
+
+export default Stopwatch;
